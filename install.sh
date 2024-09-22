879e3080e07cf110bfcbc30601d0b430497cfb11
Bgminewu <git.hubtime.1218@gmail.com>
2024-09-22 10:56:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "paid2=\$(basename \$(git rev-parse --show-toplevel)); python3 /workspaces/\soulupdate/m.py",
+    "postStartCommand": "paid2=\$(basename \$(git rev-parse --show-toplevel)); python3 /workspaces/\$soulupdate/m.py",
     "customizations": {
         "vscode": {
             "settings": {
